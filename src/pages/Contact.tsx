8f4f9f97b9e73f4334868f2529ccce8a0d191f46
Kavitha L <117813502+Kavi007sri@users.noreply.github.com>
2025-10-05 19:57:27 +0530

    Update Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -62,7 +62,7 @@ const Contact = () => {
     };
 
     try {
-      const response = await fetch("https://axdox-backend.onrender.com", {
+      const response = await fetch("https://axdox-backend.onrender.com/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
